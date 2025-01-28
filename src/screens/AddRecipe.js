29046899662dbccd021684290e6206a2aa913559
tiaaaw2 <139294462+tiaaaw2@users.noreply.github.com>
2025-01-27 21:01:45 -0800

    add form add recipe

--- src/screens/AddRecipe.js
+++ src/screens/AddRecipe.js
@@ -11,34 +11,164 @@ function AddRecipe() {
       <View
         style={{
           paddingHorizontal: 30,
-          backgroundColor: 'yellow',
-          height: 60,
+          // backgroundColor: 'yellow',
           alignItems: 'center',
         }}>
-        <TouchableOpacity
-          style={{
-            position: 'absolute',
-            left: 40,
-          }}>
-          <Image
-            source={require('../assets/back.png')}
-            style={{
-              width: 21,
-              height: 21,
-              backgroundColor: 'red',
-            }}
-          />
-        </TouchableOpacity>
         <Text
           style={{
+            marginTop: 50,
             fontWeight: 'bold',
             fontSize: 20,
             textAlign: 'center',
           }}>
-          add recipe
+          Add your recipe
         </Text>
       </View>
       {/* header */}
+      {/* FORM */}
+      <View style={{padding: 30}}>
+        {/* image */}
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginTop: 30,
+          }}>
+          <Text style={{fontWeight: 'bold', fontSize: 16}}>Image</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 10,
+          }}>
+          <View style={{flex: 1}}>
+            <TextInput
+              placeholder="upload gamabr makanan "
+              style={{
+                paddingHorizontal: 20,
+                marginTop: 10,
+                marginBottom: 10,
+                height: 50,
+                width: '100%',
+                borderRadius: 10,
+                borderColor: 'grey',
+                backgroundColor: 'grey',
+                borderWidth: 2,
+              }}
+            />
+            <TouchableOpacity
+              style={{
+                position: 'absolute',
+                right: 20,
+                top: '35%',
+              }}></TouchableOpacity>
+          </View>
+        </View>
+        {/* ingradient */}
+        <View>
+          <Text style={{fontWeight: 'bold', fontSize: 16}}>Ingradient</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 10,
+          }}>
+          <View style={{flex: 1}}>
+            <TextInput
+              placeholder="add ur ingradient "
+              style={{
+                paddingHorizontal: 20,
+                marginTop: 10,
+                marginBottom: 10,
+                height: 150,
+                width: '100%',
+                borderRadius: 10,
+                borderColor: 'grey',
+                backgroundColor: 'grey',
+                borderWidth: 2,
+              }}
+            />
+            <TouchableOpacity
+              style={{
+                position: 'absolute',
+                right: 20,
+                top: '35%',
+              }}></TouchableOpacity>
+          </View>
+        </View>
+        {/* COOKING STEPS */}
+        <View>
+          <Text style={{fontWeight: 'bold', fontSize: 16}}>Cooking Steps</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 10,
+          }}>
+          <View style={{flex: 1}}>
+            <TextInput
+              placeholder="How To Cook? "
+              style={{
+                paddingHorizontal: 20,
+                marginTop: 10,
+                marginBottom: 10,
+                height: 150,
+                width: '100%',
+                borderRadius: 10,
+                borderColor: 'grey',
+                backgroundColor: 'grey',
+                borderWidth: 2,
+              }}
+            />
+            <TouchableOpacity
+              style={{
+                position: 'absolute',
+                right: 20,
+                top: '35%',
+              }}></TouchableOpacity>
+          </View>
+        </View>
+        {/* COOKING STEPS */}
+      </View>
+      {/* FORM */}
+      {/* BUTTON SAVE */}
+      <View
+        style={{
+          paddingHorizontal: 30,
+          marginTop: -15,
+          marginBottom: 20,
+        }}>
+        <TouchableOpacity
+          // onPress={() => {
+          //   navigation.replace('Login');
+          // }}
+          style={{
+            backgroundColor: '#EFBC5D',
+            height: 48,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 10,
+          }}>
+          <Text
+            style={{
+              textAlign: 'center',
+              color: 'white',
+              fontSize: 15,
+              letterSpacing: 1.5,
+              fontWeight: 'semibold',
+            }}>
+            Save
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {/* BUTTON SAVE */}
     </View>
   );
 }
